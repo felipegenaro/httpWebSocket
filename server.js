73b7c6276754342ab7c570bbb47436914c828f28
Felipe Corniani de Genaro <felipegenaro@Felipe-Genaro-MacBook-Air.local>
2018-06-20 16:53:12 -0300

    correcao

--- server.js
+++ server.js
@@ -16,14 +16,14 @@ wsServer = new WebSocketServer({
 });
 
 // bloquear origens indefinidas. ou range de ip.
-function originIsAllowed(origin) {
-  return true;
+function originIsAllowed(request) {
+    if(request) return false;
+    else return true;
 }
-
 wsServer.on('request', function(request) {
-    if (!originIsAllowed(request.origin)) {
+    if (!originIsAllowed(request.url)) {
       request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' From Adress ' + connection.remoteAddress + ' rejected.');
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
       return;
     }
     
@@ -34,7 +34,7 @@ wsServer.on('request', function(request) {
         	var content = 'Received Message: ' + message.utf8Data + ' From: ' + connection.remoteAddress + ' at ' + (new Date());
             console.log(content);
             // connection.sendUTF(message.utf8Data);
-            log(content);
+            logFile(content);
         }
     });
     connection.on('close', function(reasonCode, description) {
@@ -43,7 +43,7 @@ wsServer.on('request', function(request) {
 });
 
 var fs = require('fs');
-function log(content){
+function logFile(content){
 	var logger = fs.createWriteStream('log.txt', {
 	  flags: 'a'
 	});
